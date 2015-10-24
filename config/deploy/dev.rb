e557a1e10252bbd031fe481de135133f9163f0dd
Josh Rogan <jjjr1122@gmail.com>
2015-10-24 18:50:23 -0400

    Update roles

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server 'droplet.joshuarogan.com', user: 'josh', roles: %w{app db web}
+server 'droplet.joshuarogan.com', user: 'josh', roles: %w{app web}
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
